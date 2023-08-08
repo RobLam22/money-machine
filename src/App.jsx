843b8cc472735c308f61bca0d1f9bdb666d9563c
rlam22 <robert.lam22@gmail.com>
2023-08-08 20:11:29 +1000

    supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 function App() {
+  const [data, setData] = useState("");
+
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY
+  );
+
+  const callUsers = async () => {
+    const { data } = await supabase.from("Users").select();
+    console.log(data);
+    setData(data);
+  };
+
+  useEffect(() => {
+    callUsers();
+  }, []);
+
   return (
     <>
       <div>
-        
+        <p>{data[0] && data[0].username}</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
