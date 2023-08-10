7573e640a85288a7e942491226bc1fbf2ef2eace
rlam22 <robert.lam22@gmail.com>
2023-08-10 20:57:00 +1000

    navbar working

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import supabase from "./supabase";
-import Home from "./screens/Home";
+import Faq from "./screens/Faq";
 import Navbar from "./Navbar";
 import Landing from "./Navbar";
+import Dashboard from "./screens/Dashboard";
 
 function App() {
   const [session, setSession] = useState(false);
@@ -25,43 +26,33 @@ function App() {
     return () => subscription.unsubscribe();
   }, []);
 
-  if (!session) {
-    return (
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route
-            path="/login"
-            element={
-              <Auth
-                supabaseClient={supabase}
-                providers={[]}
-                appearance={{
-                  theme: ThemeSupa,
-                  variables: { default: { colors: { brand: "blue" } } },
-                }}
-              />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    );
-  } else {
-    return (
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/about"
-            element={
-              <div>
-                <h1>About</h1>
-              </div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        {!session ? (
+          <Route path="/" element={<Navbar />}>
+            <Route element={<Landing />} />
+            <Route path="faq" element={<Faq />} />
+            <Route
+              path="login"
+              element={
+                <Auth
+                  supabaseClient={supabase}
+                  providers={[]}
+                  appearance={{
+                    theme: ThemeSupa,
+                    variables: { default: { colors: { brand: "blue" } } },
+                  }}
+                />
+              }
+            />
+          </Route>
+        ) : (
+          <Route path="/" element={<Dashboard />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
---
+++ src/screens/Pricing.jsx
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+import { useNavigate } from "react-router";
+import Navbar from "../Navbar.jsx";
+
+export default function Pricing() {
+  return (
+    <>
+      <div>This is the page where we can see payment plans</div>
+    </>
+  );
+}
