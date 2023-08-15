8f8b3de9929cd1f1ece04f2ba2847e05b12b6102
rlam22 <robert.lam22@gmail.com>
2023-08-15 18:45:49 +1000

    useContext and sessions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { ChakraProvider } from "@chakra-ui/react";
 import supabase from "./supabase";
 import Navbar from "./Navbar";
-import NavbarSession from "./NavbarSession";
-import HowItWorks from "./screens/HowItWorks";
 import Pricing from "./screens/Pricing";
 import Faq from "./screens/Faq";
 import Dashboard from "./screens/Dashboard";
@@ -21,92 +17,86 @@ import SocialMediaPost from "./screens/Tools/SocialMediaPost";
 import SentenceExpander from "./screens/Tools/SentenceExpander";
 import IdeaGenerator from "./screens/Tools/IdeaGenerator";
 import KeywordSuggestionsSEO from "./screens/Tools/KeywordSuggestionsSEO";
+import Home from "./screens/Home";
+import SessionsContextComp from "./SessionsContext";
+import Signup from "./screens/Signup";
 
 function App() {
-  const [session, setSession] = useState(false);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-      navigate("/");
-      // console.log(_event);
-      // if (session && !window.location.href) window.location.href = "/";
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
   return (
-    <ChakraProvider>
-      {session ? <NavbarSession /> : <Navbar session={session} />}
+    <SessionsContextComp>
+      <Navbar />
       <Routes>
-        {!session ? (
-          <>
-            <Route index path="how" element={<HowItWorks />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="features" element={<Features />} />
-            <Route path="faq" element={<Faq />} />
-            <Route
-              path="login"
-              element={
-                <>
-                  <Auth
-                    supabaseClient={supabase}
-                    // redirectTo="/"
-                    providers={[]}
-                    appearance={{
-                      theme: ThemeSupa,
-                      variables: { default: { colors: { brand: "blue" } } },
-                    }}
-                  />
-                </>
-              }
-            />
-          </>
-        ) : (
-          <>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/blog-post-writer" element={<BlogPostWriter />} />
-            <Route path="/article-generator" element={<ArticleGenerator />} />
-            <Route path="/grammar-fix" element={<GrammarFix />} />
-            <Route
-              path="/product-descriptions"
-              element={<ProductDescriptions />}
-            />
-            <Route path="/sentence-rewriter" element={<SentenceRewriter />} />
-            <Route path="/social-media-post" element={<SocialMediaPost />} />
-            <Route path="/sentence-expander" element={<SentenceExpander />} />
-            <Route path="/idea-generator" element={<IdeaGenerator />} />
-            <Route
-              path="/keyword-suggestions-seo"
-              element={<KeywordSuggestionsSEO />}
-            />
-            <Route
-              path="/*"
-              element={
+        <>
+          <Route index path="/" element={<Home />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="features" element={<Features />} />
+          <Route path="faq" element={<Faq />} />
+          <Route path="signup" element={<Signup />} />
+          <Route
+            path="signin"
+            element={
+              <>
                 <Auth
                   supabaseClient={supabase}
-                  // redirectTo="/login"
+                  // redirectTo="/"
                   providers={[]}
                   appearance={{
                     theme: ThemeSupa,
                     variables: { default: { colors: { brand: "blue" } } },
                   }}
                 />
-              }
-            />
-          </>
-        )}
+              </>
+            }
+          />
+        </>
+        <>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard/blog-post-writer"
+            element={<BlogPostWriter />}
+          />
+          <Route
+            path="/dashboard/article-generator"
+            element={<ArticleGenerator />}
+          />
+          <Route path="/dashboard/grammar-fix" element={<GrammarFix />} />
+          <Route
+            path="/dashboard/product-descriptions"
+            element={<ProductDescriptions />}
+          />
+          <Route
+            path="/dashboardsentence-rewriter"
+            element={<SentenceRewriter />}
+          />
+          <Route
+            path="/dashboard/social-media-post"
+            element={<SocialMediaPost />}
+          />
+          <Route
+            path="/dashboard/sentence-expander"
+            element={<SentenceExpander />}
+          />
+          <Route path="/dashboard/idea-generator" element={<IdeaGenerator />} />
+          <Route
+            path="/dashboard/keyword-suggestions-seo"
+            element={<KeywordSuggestionsSEO />}
+          />
+          <Route
+            path="/*"
+            element={
+              <Auth
+                supabaseClient={supabase}
+                providers={[]}
+                appearance={{
+                  theme: ThemeSupa,
+                  variables: { default: { colors: { brand: "blue" } } },
+                }}
+              />
+            }
+          />
+        </>
       </Routes>
-    </ChakraProvider>
+    </SessionsContextComp>
   );
 }
 
